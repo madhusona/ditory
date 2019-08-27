e15bf846bba11b5709b88515a43eadf4042eaf1a
Madusudhanan <madhusona@gmail.com>
2019-08-27 13:34:23 +0530

    Update contract.js

--- js/contract.js
+++ js/contract.js
@@ -10,96 +10,71 @@ if (typeof web3 !== 'undefined') {
 
 
 
-
-web3.eth.defaultAccount = web3.eth.accounts[0];
+window.ethereum.enable();
+//web3.eth.defaultAccount = '0x57054C1C6BA9b9cAdCb8AA5D7aB06FDd8EC1A8c1';
+web3.eth.defaultAccount =web3.eth.accounts[0]
 const myContract = new web3.eth.Contract([
-{
-"constant": false,
-"inputs": [
-    {
-        "name": "CI",
-        "type": "uint256"
-    },
-    {
-        "name": "UI",
-        "type": "uint256"
-    },
-    {
-        "name": "i",
-        "type": "address"
-    },
-    {
-        "name": "CH",
-        "type": "string"
-    }
-],
-"name": "createCertificate",
-"outputs": [],
-"payable": false,
-"stateMutability": "nonpayable",
-"type": "function"
-},
-{
-"constant": true,
-"inputs": [
-    {
-        "name": "",
-        "type": "uint256"
-    }
-],
-"name": "certificates",
-"outputs": [
-    {
-        "name": "",
-        "type": "uint256"
-    }
-],
-"payable": false,
-"stateMutability": "view",
-"type": "function"
-},
-{
-"constant": true,
-"inputs": [],
-"name": "getAllCertificates",
-"outputs": [
-    {
-        "name": "",
-        "type": "uint256[]"
-    }
-],
-"payable": false,
-"stateMutability": "view",
-"type": "function"
-},
-{
-"constant": true,
-"inputs": [
-    {
-        "name": "CI",
-        "type": "uint256"
-    }
-],
-"name": "getCertificate",
-"outputs": [
-    {
-        "name": "UI",
-        "type": "uint256"
-    },
-    {
-        "name": "i",
-        "type": "address"
-    },
-    {
-        "name": "CH",
-        "type": "string"
-    }
-],
-"payable": false,
-"stateMutability": "view",
-"type": "function"
-}
-], '0x701e9d6781e8e53ce24d19c31c64f4fc821951fa', {
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_no",
+				"type": "uint256"
+			},
+			{
+				"name": "_certificatehash",
+				"type": "string"
+			}
+		],
+		"name": "setCertificate",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "certificateAccts",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_no",
+				"type": "uint256"
+			}
+		],
+		"name": "getCertificate",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+], '0x85573a2afd73a062104892B2c0583F2f98eDA06A', {
 
 gasPrice: '20000000000' 
 });
